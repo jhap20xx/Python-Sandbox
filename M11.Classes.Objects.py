841830cca3b4acdac0ffa68c798a28d7d1b30892
Jhap <jhedoalinso@gmail.com>
2024-05-17 16:57:58 +0800

    Classes and objects

---
+++ M11.Classes.Objects.py
@@ -0,0 +1,16 @@
+class Character:
+    name = "name"
+    hp = "100"
+    mp = "50"
+    atk = "10"
+    lvl = "1"
+
+charone = Character()
+charone.name = "Pablo"
+print(charone.name)
+
+chartwo = Character()
+chartwo.name = "Gregorio"
+print(chartwo.name)
+
+
