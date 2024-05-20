c1359f87ed5b7c0a131b1279fa30654b2ab7834c
Jhap <jhedoalinso@gmail.com>
2024-05-20 17:19:41 +0800

    Inheritance with user input

--- OOP2.py
+++ OOP2.py
@@ -15,37 +15,6 @@
 # for person in listUser:
 #     person.intro()
 
-# class Person:
-#     def __init__(self,fname,lname):
-#         self.fname = fname
-#         self.lname = lname
-
-#     def con(self):
-#         print(self.fname + " " + self.lname + " has been added to records.")
-
-# studentRecords = []
-
-# class students(Person):
-#     def __init__(self, fname, lname):
-#         super().__init__(fname, lname)
-    
-#     def con(self):
-#         return super().con()
-
-# while True:
-#     fname = input("Enter First Name: ")
-#     lname = input("Enter Last Name: ")
-#     pup = students(fname,lname)
-#     studentRecords.append(pup)
-
-#     if fname == "exit":
-#         break
-        
-#     for stud in studentRecords:
-#         stud.con()
-
-# for x in studentRecords:
-#     print(x)
 
 class Person:
     def __init__(self, fname, lname):
@@ -65,10 +34,12 @@ class Students(Person):
         super().con()  # No need to return here
 
 while True:
+    
     fname = input("Enter First Name (or type 'exit' to quit): ")
     if fname.lower() == "exit":
         break
     lname = input("Enter Last Name: ")
+    print("Type 'exit' to exit")
     pup = Students(fname, lname)
     studentRecords.append(pup)
 
